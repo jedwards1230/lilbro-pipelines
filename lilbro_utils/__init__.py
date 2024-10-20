6ab7c6d1a47be31116166d9cdef359dcd1206189
Justin <justinedwards1230@gmail.com>
2024-10-20 13:40:20 -0400

    fix: inits

--- lilbro_utils/__init__.py
+++ lilbro_utils/__init__.py
@@ -1,2 +1,11 @@
-from main import *
-from plex import *
+import os
+import importlib
+
+# Get the directory of the current file
+current_dir = os.path.dirname(__file__)
+
+# List all Python files in the current directory
+for filename in os.listdir(current_dir):
+    if filename.endswith(".py") and filename != "__init__.py":
+        module_name = filename[:-3]  # Remove the .py extension
+        importlib.import_module(f".{module_name}", package=__name__)
