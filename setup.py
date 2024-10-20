3d4512dcb602955bd3bd4462c82e9ed3d7f5f44a
Justin <justinedwards1230@gmail.com>
2024-10-20 12:10:52 -0400

    fix: setup file

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="lilbro_pipelines",
     version="0.1.0",
-    packages=find_packages(where="lib"),
-    package_dir={"": "lib"},
+    packages=["lilbro_pipelines"],
+    package_dir={"lilbro_pipelines": "lib"},
 )
